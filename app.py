ee24500aefe896c231200958516aa843c25cb5b1
oron-sinaa <m.aanis1999@gmail.com>
2022-06-18 23:53:51 +0530

    changed folder structure

--- app.py
+++ app.py
@@ -11,15 +11,22 @@ from ui_main import Ui_MainWindow
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setupUi(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         self.connectSignalsSlots()
 
     def connectSignalsSlots(self):
-        self.action_New.triggered.connect(self.create_file)
-        self.action_Open.triggered.connect(self.open_file)
-        self.actionSave.triggered.connect(self.save_file)
-        self.actionCut.triggered.connect(self.editor.cut)
-        self.actionCopy.triggered.connect(self.editor.copy)
-        self.actionPaste.triggered.connect(self.editor.paste)
-        self.actionUndo.triggered.connect(self.editor.undo)
-        self.actionRedo.triggered.connect(self.editor.redo)
+        self.ui.action_New.triggered.connect(self.create_file)
+        self.ui.action_Open.triggered.connect(self.open_file)
+        self.ui.actionSave.triggered.connect(self.save_file)
+        self.ui.actionCut.triggered.connect(self.editor.cut)
+        self.ui.actionCopy.triggered.connect(self.editor.copy)
+        self.ui.actionPaste.triggered.connect(self.editor.paste)
+        self.ui.actionUndo.triggered.connect(self.editor.undo)
+        self.ui.actionRedo.triggered.connect(self.editor.redo)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
