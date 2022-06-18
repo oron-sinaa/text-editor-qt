d536a60cfe7da386088b9cca420fbd0c556b64a9
oron-sinaa <m.aanis1999@gmail.com>
2022-06-18 23:07:28 +0530

    updated connectSignalsSlots()

--- app.py
+++ app.py
@@ -5,3 +5,21 @@ from PyQt5.QtWidgets import (
 )
 
 from PyQt5.uic import loadUi
+
+from ui_main import Ui_MainWindow
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.connectSignalsSlots()
+
+    def connectSignalsSlots(self):
+        self.action_New.triggered.connect(self.create_file)
+        self.action_Open.triggered.connect(self.open_file)
+        self.actionSave.triggered.connect(self.save_file)
+        self.actionCut.triggered.connect(self.editor.cut)
+        self.actionCopy.triggered.connect(self.editor.copy)
+        self.actionPaste.triggered.connect(self.editor.paste)
+        self.actionUndo.triggered.connect(self.editor.undo)
+        self.actionRedo.triggered.connect(self.editor.redo)
